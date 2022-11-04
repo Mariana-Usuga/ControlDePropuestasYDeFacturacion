b7437bbbc9dbc918f78a012345dfe76f14610cd9
Mariana-Usuga <marianausugamontoya12344@gmail.com>
2022-11-03 22:08:49 -0500

    cambios

--- src/app/components/dialog-approve-proposal/dialog-approve-proposal.component.ts
+++ src/app/components/dialog-approve-proposal/dialog-approve-proposal.component.ts
@@ -1,5 +1,6 @@
 import { Component, Inject, OnInit } from '@angular/core';
 import { MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { commercialProposal } from 'src/app/models/interfaces/commercialProposal.interfaces';
 import { BusinessProposalService } from 'src/app/services/business-proposal.service';
 
 @Component({
@@ -12,8 +13,26 @@ export class DialogApproveProposalComponent implements OnInit {
   constructor( private businessProposalService: BusinessProposalService,
     @Inject(MAT_DIALOG_DATA) public proposalSee: any) { }
 
+    cambio: any = {
+      id: this.proposalSee.id,
+      cliente: this.proposalSee.cliente,
+      empresa: this.proposalSee.empresa,
+      mes: this.proposalSee.mes,
+      clienteReferencia: this.proposalSee.clienteReferencia,
+      year: this.proposalSee.year,
+      conceptoServicio: this.proposalSee.conceptoServicio,
+      tipoDeServicio: this.proposalSee.tipoDeServicio,
+      estado: "aprobado",
+      garantia: this.proposalSee.garantia,
+      moneda: this.proposalSee.moneda,
+      montoBase: this.proposalSee.montoBase,
+      montoTotal: this.proposalSee.montoTotal,
+      version: this.proposalSee.version,
+      idVersionMismaPropuesta: this.proposalSee.idVersionMismaPropuesta
+    }
+
   ngOnInit(): void {
-    this.businessProposalService.putStateOfProposal(this.proposalSee).subscribe(
+    this.businessProposalService.putStateOfProposal(this.cambio).subscribe(
       (res) => {
         console.log('res', res)
       },
