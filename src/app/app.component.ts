d702ff3aa8f5c37cdbf122e586b559fda116df93
Mariana-Usuga <marianausugamontoya12344@gmail.com>
2022-12-07 18:39:09 -0500

    cambios

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,7 +8,7 @@ import { Component } from '@angular/core';
 export class AppComponent {
   valores = []
   //title = 'ControlDePropuestasYDeFacturacion';
-  title = 'proposalControlF'
+  title = 'proposalControlF';
 
   ver(){
     console.log('lvalores', this.valores);
