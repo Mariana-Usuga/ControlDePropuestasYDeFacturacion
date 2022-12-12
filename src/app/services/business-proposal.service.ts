8d174108ef71a5e17f1d7a0fade86cce65ff6276
Mariana-Usuga <marianausugamontoya12344@gmail.com>
2022-12-12 16:57:59 -0500

    link api

--- src/app/services/business-proposal.service.ts
+++ src/app/services/business-proposal.service.ts
@@ -9,8 +9,8 @@ const EXCEL_TYPE =
 'application/vnd.openxmlformats-officedocument.spredsheetml-sheet; charset=UTF-8';
 const EXCEL_EXT = '.xlsx';
 
-const URL = 'http://119.8.153.220:8080/proposalControlV1-0.0.1'
-
+//const URL = 'http://119.8.153.220:8080/proposalControlV1-0.0.1'
+const URL = 'localhost:8080'
 @Injectable({
   providedIn: 'root'
 })
